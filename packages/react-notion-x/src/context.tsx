a111e89faea4300fb5bbdcf85fa4f32d3f2c4e5e
Travis Fischer <fisch0920@gmail.com>
2026-08-22 20:02:17 +0700

    feat: add tableOfContentsTitle config option
    (cherry picked from commit c0120f804d07086dd28612329392dbe18a2577a0)

--- packages/react-notion-x/src/context.tsx
+++ packages/react-notion-x/src/context.tsx
@@ -31,10 +31,12 @@ export interface NotionContext {
   previewImages: boolean
   forceCustomImages: boolean
   showCollectionViewDropdown: boolean
-  showTableOfContents: boolean
-  minTableOfContentsItems: number
   linkTableTitleProperties: boolean
   isLinkCollectionToUrlProperty: boolean
+
+  showTableOfContents: boolean
+  minTableOfContentsItems: number
+  tableOfContentsTitle?: string | null
 
   defaultPageIcon?: string | null
   defaultPageCover?: string | null
@@ -66,6 +68,7 @@ export interface PartialNotionContext {
 
   showTableOfContents?: boolean
   minTableOfContentsItems?: number
+  tableOfContentsTitle?: string | null
 
   defaultPageIcon?: string | null
   defaultPageCover?: string | null
